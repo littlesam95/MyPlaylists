f0d816862c43526e8f61efc30343c716c2ae9242
jiwon <littlesam95@naver.com>
2022-05-16 22:45:21 +0900

    22.05.16 Committed

--- app/src/main/java/com/bodan/myplaylists/SettingsActivity.kt
+++ app/src/main/java/com/bodan/myplaylists/SettingsActivity.kt
@@ -2,13 +2,10 @@ package com.bodan.myplaylists
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bodan.myplaylists.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_settings)
     }
 }
